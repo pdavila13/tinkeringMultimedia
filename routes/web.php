52285a37e9a870e8dd57e1641a2e2057a35bb5d5
Paolo Dávila <pdavila@iesebre.com>
2017-04-19 19:36:18 +0200

    Added view svg.blade.php with menu and route

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,11 @@ Route::group(['middleware' => 'auth'], function () {
 
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
+    Route::get('svg', function ()    {
+        $data = [];
+        return view('svg',$data);
+    })->name('svg');
+
     Route::get('CanvasClock', function ()    {
         $data = [];
         return view('CanvasClock',$data);
