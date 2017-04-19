3adc70761a5bd2c7ebceb1621b068840e18586f5
Paolo Dávila <pdavila@iesebre.com>
2017-04-19 19:58:46 +0200

    Added slider but not working :/

--- resources/views/svg.blade.php
+++ resources/views/svg.blade.php
@@ -5,13 +5,24 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://almsaeedstudio.com/themes/AdminLTE/plugins/bootstrap-slider/slider.css">
+    <link rel="stylesheet" href="https://almsaeedstudio.com/themes/AdminLTE/dist/css/skins/_all-skins.min.css">
     <title>SVG</title>
 </head>
 <body>
+
+<div class="box-body">
+    <div class="row margin">
+        <div class="col-sm-6">
+            <input type="text" value="" class="slider form-control" data-slider-min="-200" data-slider-max="200" data-slider-step="5" data-slider-value="[-100,100]" data-slider-orientation="horizontal" data-slider-selection="before" data-slider-tooltip="show" data-slider-id="red">
+        </div>
+    </div>
+</div>
 
 <svg width="100" height="100">
     <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
 </svg>
 
+<script src="https://almsaeedstudio.com/themes/AdminLTE/plugins/bootstrap-slider/bootstrap-slider.js"></script>
 </body>
 </html>
