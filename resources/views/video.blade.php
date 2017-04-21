3ccf72fd47e44d3f31cbc599880a170ee11bf38d
Paolo Dávila <pdavila@iesebre.com>
2017-04-21 16:33:24 +0200

    Implement audio

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -20,6 +20,41 @@
                     <button onclick="document.getElementById('video').volume-=0.1">Disminuir el Volumen</button>
                 </div>
                 <!-- END manual controls -->
+
+                <hr>
+
+                <div class="test">Back in black:</div>
+
+                <audio id="demo" src="https://upload.wikimedia.org/wikipedia/en/4/45/ACDC_-_Back_In_Black-sample.ogg" preload="auto" controls></audio>
+
+                <div>
+                    <button onclick="document.getElementById('demo').play()">Reproducir el Audio</button>
+                    <button onclick="document.getElementById('demo').pause()">Pausar el Audio</button>
+                    <button onclick="document.getElementById('demo').volume+=0.1">Aumentar el Volumen</button>
+                    <button onclick="document.getElementById('demo').volume-=0.1">Disminuir el Volumen</button>
+                </div>
+
+                <div class="test">Audio Another day in paradise:</div>
+                <audio id="demo2" src="https://upload.wikimedia.org/wikipedia/en/9/9f/Sample_of_%22Another_Day_in_Paradise%22.ogg" preload="auto" controls></audio>
+
+                <div>
+                    <button onclick="document.getElementById('demo2').play()">Reproducir el Audio</button>
+                    <button onclick="document.getElementById('demo2').pause()">Pausar el Audio</button>
+                    <button onclick="document.getElementById('demo2').volume+=0.1">Aumentar el Volumen</button>
+                    <button onclick="document.getElementById('demo2').volume-=0.1">Disminuir el Volumen</button>
+                </div>
+
+                <div class="test">Audio Bullet in the head:</div>
+                <audio id="demo3" src="https://upload.wikimedia.org/wikipedia/en/1/17/Bullet_In_The_Head.ogg" preload="auto" controls></audio>
+
+                <div>
+                    <button onclick="document.getElementById('demo3').play()">Reproducir el Audio</button>
+                    <button onclick="document.getElementById('demo3').pause()">Pausar el Audio</button>
+                    <button onclick="document.getElementById('demo3').volume+=0.1">Aumentar el Volumen</button>
+                    <button onclick="document.getElementById('demo3').volume-=0.1">Disminuir el Volumen</button>
+                </div>
+
+                <a href="https://developer.mozilla.org/es/docs/Web/HTML/Usando_audio_y_video_con_HTML5">Més info</a>
 			</div>
 		</div>
 	</div>
