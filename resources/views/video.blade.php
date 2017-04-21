804ffac045bd1bf4eec0941fee20d339437a58f4
Paolo Dávila <pdavila@iesebre.com>
2017-04-21 16:23:39 +0200

    Added manual controls in video

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -12,6 +12,14 @@
 
                 <video id="video" src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" preload="auto" controls></video>
 
+                <!-- Manual controls -->
+                <div>
+                    <button onclick="document.getElementById('video').play()">Reproducir video</button>
+                    <button onclick="document.getElementById('video').pause()">Pausar el video</button>
+                    <button onclick="document.getElementById('video').volume+=0.1">Aumentar el Volumen</button>
+                    <button onclick="document.getElementById('video').volume-=0.1">Disminuir el Volumen</button>
+                </div>
+                <!-- END manual controls -->
 			</div>
 		</div>
 	</div>
