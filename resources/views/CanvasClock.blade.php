96981f97074cf222dc577a187beb695e56031e6f
Paolo Dávila <pdavila@iesebre.com>
2017-06-06 10:25:27 +0200

    Part III - Draw Clock Numbers

--- resources/views/CanvasClock.blade.php
+++ resources/views/CanvasClock.blade.php
@@ -24,6 +24,7 @@
         ctx.fill();
 
         drawFace(ctx, radius);
+        drawNumbers(ctx, radius);
     }
 
     function drawFace(ctx, radius) {
@@ -46,6 +47,24 @@
         ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
         ctx.fillStyle = '#333';
         ctx.fill();
+    }
+
+    function drawNumbers(ctx, radius) {
+        var ang;
+        var num;
+        ctx.font = radius*0.15 + "px arial";
+        ctx.textBaseline="middle";
+        ctx.textAlign="center";
+        for(num= 1; num < 13; num++){
+            ang = num * Math.PI / 6;
+            ctx.rotate(ang);
+            ctx.translate(0, -radius*0.85);
+            ctx.rotate(-ang);
+            ctx.fillText(num.toString(), 0, 0);
+            ctx.rotate(ang);
+            ctx.translate(0, radius*0.85);
+            ctx.rotate(-ang);
+        }
     }
 </script>
 </body>
