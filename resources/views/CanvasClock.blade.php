7c213d5571e5e0c570c15a2636f1bf774f95bc87
Paolo Dávila <pdavila@iesebre.com>
2017-06-06 10:27:22 +0200

    Part V - Start the Clock

--- resources/views/CanvasClock.blade.php
+++ resources/views/CanvasClock.blade.php
@@ -15,8 +15,9 @@
     var ctx = canvas.getContext("2d");
     var radius = canvas.height / 2;
     ctx.translate(radius, radius);
-    radius = radius * 0.90
-    drawClock();
+    radius = radius * 0.90;
+
+    setInterval(drawClock, 1000);
 
     function drawClock() {
         ctx.arc(0, 0, radius, 0 , 2*Math.PI);
